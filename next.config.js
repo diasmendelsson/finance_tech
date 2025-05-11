e05b68f2f5ac2d59073b1dfc4bbc57f17f517ff0
diasmendelsson <mendersson@hotmail.com>
2025-05-10 23:28:06 -0300

    Mudado variavel de ambiente e arquivo do banco

--- next.config.js
+++ next.config.js
@@ -7,3 +7,14 @@ module.exports = {
       },
     },
   }
+
+
+  module.exports = {
+  env: {
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_HOST: process.env.DB_HOST,
+    DB_NAME: process.env.DB_NAME,
+    DB_PORT: process.env.DB_PORT,
+  },
+};
